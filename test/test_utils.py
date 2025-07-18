7151a1ac83770c92102fad0ef59ff7c8ec4cdda8
ruebntt <ruebennett.chereshnua@gmail.com>
2025-07-18 12:29:26 +0500

    Update test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,1 +1,9 @@
+from app.utils import parse_array_from_str, array_to_str
 
+def test_parse_array_from_str():
+    assert parse_array_from_str("3,2,1") == [3, 2, 1]
+    assert parse_array_from_str("10,20") == [10, 20]
+
+def test_array_to_str():
+    assert array_to_str([3, 2, 1]) == "3,2,1"
+    assert array_to_str([]) == ""
